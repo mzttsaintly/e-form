8b808ac9e004e7abe9b32a39f60794edac5d3b98
sai <unuser@163.com>
2023-07-27 17:28:33 +0800

    新建

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <el-row class="pcHeadBox" :span="24"></el-row>
+  <el-row class="mainBox" :span="24">
+    <el-col class="sideBox" :span="0" :md="3"></el-col>
+    <el-col class="showBox" :span="24" :md="19"></el-col>
+  </el-row>
+</template>
+
+<style scoped></style>
